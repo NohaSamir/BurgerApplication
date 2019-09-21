0647a49744c8278b337279c018e4e7d9e3ce96da
NohaSamir <eng.nohasamirsaad@gmail.com>
2019-09-21 17:04:51 +0200

    commit

--- app/src/main/java/com/example/burgerapplication/ListAdapter.kt
+++ app/src/main/java/com/example/burgerapplication/ListAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.burgerapplication
 
+import android.graphics.drawable.AnimatedVectorDrawable
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.list_item.view.*
 
@@ -15,6 +18,25 @@ import kotlinx.android.synthetic.main.list_item.view.*
  * 3- translate burger from bottom of the cell
  * 4- translate new icon and Zoom it
  */
+
+//ToDo : We need to make the cells rounded while scrolling
+
+// ToDo 1 : We will create 4 rectangles with the 4 colors
+// Create rectangle vector using illustrator and export it as svg file
+// and then we will open VectAlign (https://github.com/bonnyfone/vectalign) to get a morphing path
+// copy morphing path in drawable/rectangle_   .xml
+
+// ToDo 2 : We will create a rounded rectangle with the same previous way
+// and then create rectangle path converter in animator/rectangle_path_converter.xml
+
+// ToDo 3 : Create Object animator
+// that will connect the vector with it's animation in drawable/rectangle_xxx_animator.xml
+
+// ToDo 4 : Now we need to make the background as an Image view
+// Add backgroundImageView in list-item.xml
+
+// ToDo 5: Edit setCellBackground() fun
+
 
 class ListAdapter : RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
 
@@ -38,7 +60,7 @@ class ListAdapter : RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
 
         private val context = itemView.context
 
-        private val background = itemView.rootLayout
+        private val backgroundImageView = itemView.backgroundImageView
 
         //Load zoom animation
         private val textZoomAnim = AnimationUtils.loadAnimation(context, R.anim.zoom_in)
@@ -66,15 +88,23 @@ class ListAdapter : RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
         private fun setCellBackground(position: Int) {
             val temp = position % 4
 
-            background.setBackgroundResource(
+            backgroundImageView.setImageDrawable(
                 when (temp) {
-                    0 -> R.color.backgroundGreen
-                    1 -> R.color.backgroundRedDark
-                    2 -> R.color.backgroundBrown
-                    3 -> R.color.backgroundTan
-                    else -> 0
+                    0 -> ContextCompat.getDrawable(context , R.drawable.rectangle_green_animator)
+                    1 -> ContextCompat.getDrawable(context , R.drawable.rectangle_red_animator)
+                    2 -> ContextCompat.getDrawable(context , R.drawable.rectangle_brown_animator)
+                    3 -> ContextCompat.getDrawable(context , R.drawable.rectangle_tan_animator)
+                    else -> ContextCompat.getDrawable(context , R.drawable.rectangle_green_animator)
                 }
             )
+
+            val animationDrawable = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                backgroundImageView.drawable as AnimatedVectorDrawable
+
+            } else {
+                TODO("VERSION.SDK_INT < LOLLIPOP")
+            }
+            animationDrawable.start()
         }
 
         /**
@@ -100,7 +130,7 @@ class ListAdapter : RecyclerView.Adapter<ListAdapter.ListViewHolder>() {
             //New Icon Animation
             newMoveAnim.startOffset = 200
             itemView.newImageView.startAnimation(newMoveAnim)
-            newMoveAnim.setAnimationListener(object : Animation.AnimationListener{
+            newMoveAnim.setAnimationListener(object : Animation.AnimationListener {
                 //On animation end start zooming
                 override fun onAnimationEnd(p0: Animation?) {
                     //Zoom New icon
